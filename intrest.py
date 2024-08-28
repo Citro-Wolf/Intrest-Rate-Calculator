6e182daf2a4ee247636e6475bcc2e2e6e9b3b5a5
Citro-Wolf <griffinbr@bentonvillek12.org>
2024-08-28 13:25:17 -0500

    Add files via upload

---
+++ intrest.py
@@ -0,0 +1,9 @@
+# You can edit these if you want to use this for something else, it will work for basically whatever you want to get a increasing interest rate lol-
+starting_point = 1000
+interest_rate = 0.05
+# Below is the year count for how many years you wanna calculate. 
+interest_rate_after = 5
+# Below is the code for the calculation, I wouldnt mess with it if you didnt know what youre doing. :)
+print("The interest rate goes up by", starting_point * interest_rate,"every year.")
+print("The interest gained in",interest_rate_after,"Years is,",starting_point * interest_rate * interest_rate_after)
+# Made by Brian Griffin as of 28/08/2024 
